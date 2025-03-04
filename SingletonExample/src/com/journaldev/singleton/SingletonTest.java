1d0cfcf8e37114d548fa67c52c21c5c79dc399d0
H82M19\Student <Student>
2025-03-04 19:34:28 +0700

    StaticBlockSingleton

--- SingletonExample/src/com/journaldev/singleton/SingletonTest.java
+++ SingletonExample/src/com/journaldev/singleton/SingletonTest.java
@@ -3,16 +3,22 @@ package com.journaldev.singleton;
 public class SingletonTest {
     public static void main(String[] args) {
        
+        // EagerInitializedSingleton
         EagerInitializedSingleton singleton = EagerInitializedSingleton.getInstance();
-        
-        
         System.out.println(singleton);
 
-      
         EagerInitializedSingleton singleton2 = EagerInitializedSingleton.getInstance();
         System.out.println(singleton2);
 
-    
-        System.out.println("Are both instances same? " + (singleton == singleton2));
+        System.out.println("Are both EagerInitializedSingleton instances same? " + (singleton == singleton2));
+
+        // StaticBlockSingleton
+        StaticBlockSingleton singleton1 = StaticBlockSingleton.getInstance();
+        System.out.println(singleton1);
+
+        StaticBlockSingleton singleton3 = StaticBlockSingleton.getInstance();
+        System.out.println(singleton3);
+
+        System.out.println("Are both StaticBlockSingleton instances same? " + (singleton1 == singleton3));
     }
 }
